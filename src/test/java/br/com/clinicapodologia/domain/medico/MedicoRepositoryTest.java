1eae76624f138e771741ad7e55a33b775d845b91
mdroslucas <lucas-medeiros@live.com>
2023-09-26 19:49:20 -0300

    adding new test method

--- src/test/java/br/com/clinicapodologia/domain/medico/MedicoRepositoryTest.java
+++ src/test/java/br/com/clinicapodologia/domain/medico/MedicoRepositoryTest.java
@@ -49,16 +49,14 @@ class MedicoRepositoryTest {
 
     @Test
     @DisplayName("Deveria devolver medico quando ele estiver disponível na data")
-    void escolherMedicoAleatorioLivreNaData() {
+    void escolherMedicoAleatorioLivreNaDataQuandoEleEstiverLivre() {
 
         var proximaSegundaAs14 = LocalDate.now().with(TemporalAdjusters.next(DayOfWeek.MONDAY)).atTime(10, 0);
 
         var medico = cadastrarMedico("Medico", "medico@voll.med", "123456", Especialidade.CARDIOLOGIA);
-        var paciente = cadastrarPaciente("Paciente", "paciente@email.com", "00000000000");
-        cadastrarConsulta(medico, paciente, proximaSegundaAs14);
 
         var medicoLivre = medicoRepository.escolherMedicoAleatorioLivreNaData(Especialidade.CARDIOLOGIA, proximaSegundaAs14);
-        assertThat(medicoLivre).isNull();
+        assertThat(medicoLivre).isEqualTo(medico);
     }
 
     private void cadastrarConsulta(Medico medico, Paciente paciente, LocalDateTime data) {
