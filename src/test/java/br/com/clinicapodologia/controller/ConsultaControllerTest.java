844a91ee6504d013ba419d16bb029ab3051b4957
mdroslucas <lucas-medeiros@live.com>
2023-09-28 14:59:14 -0300

    all tests have passed

--- src/test/java/br/com/clinicapodologia/controller/ConsultaControllerTest.java
+++ src/test/java/br/com/clinicapodologia/controller/ConsultaControllerTest.java
@@ -1,25 +1,48 @@
 package br.com.clinicapodologia.controller;
 
+import br.com.clinicapodologia.domain.consultas.AgendaDeConsultas;
+import br.com.clinicapodologia.domain.consultas.DadosAgendamentoConsulta;
+import br.com.clinicapodologia.domain.consultas.DadosDetalhamentoConsulta;
+import br.com.clinicapodologia.domain.medico.Especialidade;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.InstanceOfAssertFactories.LOCAL_DATE;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@AutoConfigureJsonTesters
 class ConsultaControllerTest {
 
     @Autowired
     private MockMvc mvc;
+
+    @Autowired
+    private JacksonTester<DadosAgendamentoConsulta> dadosAgendamentoConsultaJson;
+
+    @Autowired
+    private JacksonTester<DadosDetalhamentoConsulta> dadosDetalhamentoConsulta;
+
+    @MockBean
+    private AgendaDeConsultas agendaDeConsultas;
 
 
     @Test
@@ -29,6 +52,32 @@ class ConsultaControllerTest {
         var response = mvc.perform(post("/consultas")).andReturn().getResponse();
 
         assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
+
+    @Test
+    @DisplayName("Deve retornar codigo http 200 quando as informações estão válidas.")
+    @WithMockUser
+    void agendar_cenario2() throws Exception {
+        var data = LocalDateTime.now().plusHours(1);
+        var especialidade = Especialidade.CARDIOLOGIA;
+
+        var dadosDetalhamento = new DadosDetalhamentoConsulta(null, 1l, 1l, data);
+
+        when(agendaDeConsultas.agendar(any())).thenReturn(dadosDetalhamento);
+
+        var response = mvc.perform(post("/consultas")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(dadosAgendamentoConsultaJson.write(
+                        new DadosAgendamentoConsulta(1l, 2l, data ,especialidade, null)
+                ).getJson())
+        ).andReturn().getResponse();
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+
+        var jsonEsperado = dadosDetalhamentoConsulta.write(dadosDetalhamento).getJson();
+
+        assertThat(response.getContentAsString()).isEqualTo(jsonEsperado);
     }
 
 }
